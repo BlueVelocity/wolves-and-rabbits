a8d3c3b8758b1db4a6a622d9ac9c98c314663e99
BlueVelocity <josephrodney@hotmail.com>
2023-11-03 13:15:32 -0400

    Assign coordinate values to tiles

--- script.js
+++ script.js
@@ -21,6 +21,11 @@ function generateTerrain() {
 
     trees = generateTrees()
 
+    function assignCoordinates(tile, x, y) {
+        tile.setAttribute('data-coordinate-x', `${x}`)
+        tile.setAttribute('data-coordinate-y', `${y}`)
+    }
+
     for (y = 0; y < gridSize; y++) {
         row = document.createElement('div')
         row.setAttribute('id', `rowNo${y}`)
@@ -34,9 +39,11 @@ function generateTerrain() {
             if (isTreeTile == true) {
                 let treeTile = document.createElement('div')
                 treeTile.setAttribute('class', 'tree-tile')
+                assignCoordinates(treeTile, x, y)
                 row.appendChild(treeTile)
             } else {
                 let emptyTile = document.createElement('div')
+                assignCoordinates(emptyTile, x, y)
                 row.appendChild(emptyTile)
             }
         }
